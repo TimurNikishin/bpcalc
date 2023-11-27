92fc392a9865dbd58b9887c20504cc8d815bcf98
timur <TimurNikisin@ntcyber.net>
2023-11-27 21:22:15 +0000

    Optimizing Unit tests

--- UnitTest/test_bpcalc.py
+++ UnitTest/test_bpcalc.py
@@ -1,38 +1,25 @@
+response_title = b"<title>Blood Pressure Category Calculator</title>"
+response_low = b"Low blood pressure"
+response_ideal = b"Ideal blood pressure"
+response_prehigh = b"Pre-High blood pressure"
+response_high = b"High blood pressure"
+response_invalid = b"Submitted invalid values"
+
+
 def test_page_title(client):
     response = client.get("/")
-    assert b"<title>Blood Pressure Category Calculator</title>" in response.data
+    assert response_title in response.data
     
-def test_submission_low(client):
-    response = client.post("/", data={"systolic": "70", "diastolic": "40"})
-
-    assert b"Low blood pressure" in response.data
-
-def test_submission_ideal(client):
+def test_submission(client):
     response = client.post("/", data={"systolic": "110", "diastolic": "70"})
-
-    assert b"Ideal blood pressure" in response.data
-
-def test_submission_prehigh(client):
+    assert response_ideal in response.data
     response = client.post("/", data={"systolic": "130", "diastolic": "85"})
-
-    assert b"Pre-High blood pressure" in response.data
-
-def test_submission_high(client):
+    assert response_prehigh in response.data
     response = client.post("/", data={"systolic": "170", "diastolic": "95"})
-
-    assert b"High blood pressure" in response.data
-
-def test_submission_invalid_1(client):
+    assert response_high in response.data
     response = client.post("/", data={"systolic": "200", "diastolic": "95"})
-
-    assert b"Submitted invalid values" in response.data
-
-def test_submission_invalid_2(client):
+    assert response_invalid in response.data
     response = client.post("/", data={"systolic": "120", "diastolic": "35"})
-
-    assert b"Submitted invalid values" in response.data
-    
-def test_submission_invalid_3(client):
+    assert response_invalid in response.data
     response = client.post("/", data={"systolic": "180", "diastolic": "110"})
-
-    assert b"Submitted invalid values" in response.data
+    assert response_invalid in response.data
