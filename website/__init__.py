adde3c52634288efde049c2dbbbdb746550bae13
timur <TimurNikisin@ntcyber.net>
2023-11-27 21:06:45 +0000

    remove csrf

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
 import os
 
 
 def create_app():
     application = Flask(__name__)
     application.secret_key = os.urandom(24)
-    csrf = CSRFProtect()
-    csrf.init_app(application)
-    
+       
     from .views import views
     
     application.register_blueprint(views, url_prefix='/')
