d70bc6bfab60a9ae7a63c1c8ed32f8a2c1e33bfa
timur <TimurNikisin@ntcyber.net>
2023-11-27 20:45:12 +0000

    adding CSRF Protection

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 import os
 
 
 def create_app():
     application = Flask(__name__)
     application.secret_key = os.urandom(24)
-    application.config['WTF_CSRF_ENABLED'] = True
+    csrf = CSRFProtect()
+    csrf.init_app(application)
     
     from .views import views
     
