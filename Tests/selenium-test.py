196f9488181f8b1c73a8c293102e493baf8fdb36
timur <TimurNikisin@ntcyber.net>
2023-12-03 00:26:48 +0000

    Testing Selenium

--- Tests/selenium-test.py
+++ Tests/selenium-test.py
@@ -1,25 +1,45 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService 
+#from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 TEST_URL = "http://bpcalcqa.ntcyber.net"
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.headless = True
 
-driver.get(TEST_URL)
+# options.add_experimental_option("detach", True)
 
-systolic = driver.find_element(By.ID, "systolic")
-systolic.send_keys("110")
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+    driver.get(TEST_URL)
+    systolic = driver.find_element(By.ID, "systolic")
+    systolic.send_keys("110")
 
-diastolic = driver.find_element(By.ID, "diastolic")
-diastolic.send_keys("60")
+    diastolic = driver.find_element(By.ID, "diastolic")
+    diastolic.send_keys("60")
+    diastolic.send_keys(Keys.RETURN)
 
-diastolic.send_keys(Keys.RETURN)
+    result = driver.find_element(By.CLASS_NAME, "alert").get_attribute('innerHTML')
+    try:
+        assert 'Ideal blood pressure' in result
+        print('E2E test passed')
+    except AssertionError:
+        print('E2E test failed')
 
-result = driver.find_element(By.CLASS_NAME, "alert").get_attribute('innerHTML')
 
-try:
-    assert 'Ideal blood pressure' in result
-    print('E2E test passed')
-except AssertionError:
-    print('E2E test failed')
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+#driver = webdriver.Chrome()
+
+#driver.get(TEST_URL)
+
+
+
+# if 'Ideal blood pressurre' in result:
+#     print("OK")
+
+# links = driver.find_element(By.ID, 'systolic')
+# for link in links:
+#      print(link.get_attribute('min'))
+    
