5d4b8065eb8af8e0fd6f9f9b7dbb54ddeba542ea
timur <TimurNikisin@ntcyber.net>
2023-12-03 01:24:44 +0000

    testing Selenium E2E test

--- Tests/selenium-test.py
+++ Tests/selenium-test.py
@@ -1,8 +1,6 @@
 import sys
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService 
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
