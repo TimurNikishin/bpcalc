57d2b0ab2866767e284830073e32af6d802a3a2b
timur <TimurNikisin@ntcyber.net>
2023-12-03 18:06:49 +0000

    changng bpcalc function to fix complexity detected by SonarCloud

--- website/modules.py
+++ website/modules.py
@@ -19,42 +19,5 @@ def bpcalc(systolic, diastolic):
         return 'high'
     elif 140 <= systolic <= 190 and 40 <= diastolic <= 100:
         return 'high'
-
-    return 'invalid'
-
-
-# def bpcalc(systolic, diastolic):
-#     if 70 <= systolic < 90: 
-#         if 40 <= diastolic < 60:
-#             return('low')
-#         elif 60 <= diastolic < 80:
-#             return('ideal')
-#         elif 80 <= diastolic < 90:
-#             return('pre-high')
-#         elif 90 <= diastolic <= 100:
-#             return('high')
-#         else:
-#             return('invalid')
-#     elif 90 <= systolic < 120:
-#         if 40 <= diastolic < 80:
-#             return('ideal')
-#         elif 80 <= diastolic < 90:
-#             return('pre-high')
-#         elif 90 <= diastolic <= 100:
-#             return('high')
-#         else:
-#             return('invalid')
-#     elif 120 <= systolic < 140:
-#         if 40 <= diastolic < 90:
-#             return('pre-high')
-#         elif 90 <= diastolic <= 100:
-#             return('high')
-#         else:
-#             return('invalid')
-#     elif 140 <= systolic <= 190:
-#         if 40 <= diastolic <= 100:
-#             return('high')
-#         else:
-#             return('invalid')
-#     else:
-#         return('invalid')
+    else:
+        return 'invalid'
