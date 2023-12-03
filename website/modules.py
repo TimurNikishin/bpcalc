3479db7ba81c9a8fadd9dbff005c01291892eb02
timur <TimurNikisin@ntcyber.net>
2023-12-03 17:00:23 +0000

    changng bpcalc function to fix complexity detected by SonarCloud

--- website/modules.py
+++ website/modules.py
@@ -1,35 +1,60 @@
 def bpcalc(systolic, diastolic):
-    if 70 <= systolic < 90: 
-        if 40 <= diastolic < 60:
-            return('low')
-        elif 60 <= diastolic < 80:
-            return('ideal')
-        elif 80 <= diastolic < 90:
-            return('pre-high')
-        elif 90 <= diastolic <= 100:
-            return('high')
-        else:
-            return('invalid')
-    elif 90 <= systolic < 120:
-        if 40 <= diastolic < 80:
-            return('ideal')
-        elif 80 <= diastolic < 90:
-            return('pre-high')
-        elif 90 <= diastolic <= 100:
-            return('high')
-        else:
-            return('invalid')
-    elif 120 <= systolic < 140:
-        if 40 <= diastolic < 90:
-            return('pre-high')
-        elif 90 <= diastolic <= 100:
-            return('high')
-        else:
-            return('invalid')
-    elif 140 <= systolic <= 190:
-        if 40 <= diastolic <= 100:
-            return('high')
-        else:
-            return('invalid')
-    else:
-        return('invalid')
+    if 70 <= systolic < 90 and 40 <= diastolic < 60:
+        return 'low'
+    elif 70 <= systolic < 90 and 60 <= diastolic < 80:
+        return 'ideal'
+    elif 70 <= systolic < 90 and 80 <= diastolic < 90:
+        return 'pre-high'
+    elif 70 <= systolic < 90 and 90 <= diastolic <= 100:
+        return 'high'
+    elif 90 <= systolic < 120 and 40 <= diastolic < 80:
+        return 'ideal'
+    elif 90 <= systolic < 120 and 80 <= diastolic < 90:
+        return 'pre-high'
+    elif 90 <= systolic < 120 and 90 <= diastolic <= 100:
+        return 'high'
+    elif 120 <= systolic < 140 and 40 <= diastolic < 90:
+        return 'pre-high'
+    elif 120 <= systolic < 140 and 90 <= diastolic <= 100:
+        return 'high'
+    elif 140 <= systolic <= 190 and 40 <= diastolic <= 100:
+        return 'high'
+
+    return 'invalid'
+
+
+# def bpcalc(systolic, diastolic):
+#     if 70 <= systolic < 90: 
+#         if 40 <= diastolic < 60:
+#             return('low')
+#         elif 60 <= diastolic < 80:
+#             return('ideal')
+#         elif 80 <= diastolic < 90:
+#             return('pre-high')
+#         elif 90 <= diastolic <= 100:
+#             return('high')
+#         else:
+#             return('invalid')
+#     elif 90 <= systolic < 120:
+#         if 40 <= diastolic < 80:
+#             return('ideal')
+#         elif 80 <= diastolic < 90:
+#             return('pre-high')
+#         elif 90 <= diastolic <= 100:
+#             return('high')
+#         else:
+#             return('invalid')
+#     elif 120 <= systolic < 140:
+#         if 40 <= diastolic < 90:
+#             return('pre-high')
+#         elif 90 <= diastolic <= 100:
+#             return('high')
+#         else:
+#             return('invalid')
+#     elif 140 <= systolic <= 190:
+#         if 40 <= diastolic <= 100:
+#             return('high')
+#         else:
+#             return('invalid')
+#     else:
+#         return('invalid')
